fa8305eed7bfd08c73dbf88419411cd48b47e3c6
Tomek Kozlowski <tkozlowski007@gmail.com>
2015-05-16 12:40:35 +0800

    add angular staff

---
+++ app/js/controllers.js
@@ -0,0 +1,8 @@
+angular.module('OneDayTrip.controllers', []).
+  controller('tripsController', function($scope) {
+    $scope.tripList = [
+      {name: "trip1"},
+      {name: "trip2"}
+    ];
+  }
+)
