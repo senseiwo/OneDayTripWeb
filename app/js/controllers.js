3c978f280adcc704b8b680f83a847072beab8b71
kevincantstop <kevincantstop@gmail.com>
2015-05-16 17:04:25 +0800

    added fake data service

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,73 +1,17 @@
 angular.module('OneDayTrip.controllers', [])
-.controller('tripsController', function($scope, oneDayTripService) {
+.controller('tripsController', function($scope, oneDayTripService,oneTripFakeData) {
 
     oneDayTripService.getTrips().success(function(resp) {
         //$scope = response.???? 
     })
-    $scope.tripList =
-    [
-     {
-       id:"1",
-       name:"Fantastic place for boys",
-       points:
-        [{
-        id:"1",
-        type:"hist",
-        coordinate:{lon:"114.24270629882812",lat:"22.741989621606027"},
-        comments:"this place is awesome!"
-        },
-        {
-        id:"2",
-        type:"hist",
-        coordinate:{lon:"114.28321838378906",lat:"22.730907082662497"},
-        comments:"this place is not so good!"
-        },
-        {
-        id:"3",
-        type:"hist",
-        coordinate:{lon:"114.3460464477539",lat:"22.689181188747252"},
-        comments:"hate it!!!!"
-        }
-        ]
-     },
-     {
-       id:"2",name:"For lesbians",
-       points:
-        [{
-        id:"1",
-        type:"hist",
-        coordinate:{lon:"114.35729026794434",lat:"22.654017060763145"},
-        comments:"this place is awesome!"
-        },
-        {
-        id:"2",
-        type:"hist",
-        coordinate:{lon:"114.4717025756836",lat:"22.5737552844127"},
-        comments:"this place is not so good!"
-        },
-        {
-        id:"3",
-        type:"hist",
-        coordinate:{lon:"114.51152801513672",lat:"22.44879309308878"},
-        comments:"hate it!!!!"
-        }
-        ]
-     }
-    ];
+    $scope.tripList = oneTripFakeData.getFakePoints();
+    
   }
 )
-.controller('paramsController',function($scope,oneDayTripService){
+.controller('paramsController',function($scope,oneDayTripService, oneTripData){
     
-    $scope.activities=[
-        {'key':'L','text':'Low'},
-        {'key':'M','text':'Mid'},
-        {'key':'H','text':'High'}
-    ];
-    
-    $scope.topics=[
-        {'key':1,'text':'History'},
-        {'key':2,'text':'Shopping'}
-    ];
+    $scope.activities = oneTripData.getActivities();
+    $scope.topics = oneTripData.getTopics();
      
     $scope.activity = $scope.activities[0];
     $scope.selected_topics = [$scope.topics[0]];
--- app/js/services.js
+++ app/js/services.js
@@ -30,4 +30,79 @@ angular.module('OneDayTrip.services', []).
     }
     
     return tripApi;
-  });
+  })
+  .factory('oneTripData',function(){
+      var data={
+          getActivities:function(){
+              return [
+                        {'key':'L','text':'Low'},
+                        {'key':'M','text':'Mid'},
+                        {'key':'H','text':'High'}
+                    ];
+          },
+          getTopics:function(){
+             return [
+                        {'key':1,'text':'History'},
+                        {'key':2,'text':'Shopping'}
+                    ];
+          }
+      }
+      return data;
+  })
+  .factory('oneTripFakeData',function(){
+      var data={
+          getFakePoints:function(){
+              var points =[
+                {
+       id:"1",
+       name:"Fantastic place for boys",
+       points:
+        [{
+        id:"1",
+        type:"hist",
+        coordinate:{lon:"114.24270629882812",lat:"22.741989621606027"},
+        comments:"this place is awesome!"
+        },
+        {
+        id:"2",
+        type:"hist",
+        coordinate:{lon:"114.28321838378906",lat:"22.730907082662497"},
+        comments:"this place is not so good!"
+        },
+        {
+        id:"3",
+        type:"hist",
+        coordinate:{lon:"114.3460464477539",lat:"22.689181188747252"},
+        comments:"hate it!!!!"
+        }
+        ]
+     },
+     {
+       id:"2",name:"For lesbians",
+       points:
+        [{
+        id:"1",
+        type:"hist",
+        coordinate:{lon:"114.35729026794434",lat:"22.654017060763145"},
+        comments:"this place is awesome!"
+        },
+        {
+        id:"2",
+        type:"hist",
+        coordinate:{lon:"114.4717025756836",lat:"22.5737552844127"},
+        comments:"this place is not so good!"
+        },
+        {
+        id:"3",
+        type:"hist",
+        coordinate:{lon:"114.51152801513672",lat:"22.44879309308878"},
+        comments:"hate it!!!!"
+        }
+        ]
+     }
+    ];  
+    return points;
+          }
+      }
+      return data;
+  })
