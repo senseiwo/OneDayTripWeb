1fdc6456643e33d1dd0d9fb4e88c12ad52506daa
kevincantstop <kevincantstop@gmail.com>
2015-05-17 00:47:17 +0800

    fake data commit

--- app/js/services.js
+++ app/js/services.js
@@ -142,33 +142,33 @@ angular.module('OneDayTrip.services', []).
                 "trips": [
                     {
                         "id": 1,
-                        "duration": '12h',
-                        "totalCost": '500',
+                        "duration": null,
+                        "totalCost": 500,
                         "attractions": [
                             {
                                 "order": 1,
-                                "name": null,
+                                "name": 'Epam',
                                 "imageURL": null,
                                 "rating": 0,
                                 "timing": 0,
                                 "price": null,
                                 "attractionType": null,
                                 "coordinate": {
-                                    "xCoordinate": "47.6677292",
-                                    "yCoordinate": "-122.37728820000001"
+                                    "xCoordinate": "113.955133",
+                                    "yCoordinate": "22.533188"
                                 }
                             },
                             {
                                 "order": 2,
-                                "name": null,
-                                "imageURL": null,
-                                "rating": 0,
-                                "timing": 0,
-                                "price": null,
-                                "attractionType": null,
+                                "name": "Coco park",
+                                "imageURL": "http://startinchina.com/shenzhen/shopping/coco_park.html",
+                                "rating": 3,
+                                "timing": 4,
+                                "price": 500,
+                                "attractionType": "SHOPPING",
                                 "coordinate": {
-                                    "xCoordinate": "32.715738",
-                                    "yCoordinate": "-117.16108380000003"
+                                    "xCoordinate": "113.957307",
+                                    "yCoordinate": "22.530676"
                                 }
                             }
                         ]
