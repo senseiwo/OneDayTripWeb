458734d2e55b877ccbe826125c5b8e5c686725b0
kevincantstop <kevincantstop@gmail.com>
2015-05-16 15:05:48 +0800

    added controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,8 +1,15 @@
-angular.module('OneDayTrip.controllers', []).
-  controller('tripsController', function($scope) {
+angular.module('OneDayTrip.controllers', [])
+.controller('tripsController', function($scope) {
     $scope.tripList = [
       {name: "trip1", text: "bla bla bla"},
       {name: "trip2", text: "bla bla bla"}
     ];
   }
 )
+.controller('paramsController',function($scope){
+    $scope.activities=[
+        {'key':1,'text':'Low'},
+        {'key':2,'text':'Mid'},
+        {'key':3,'text':'High'}
+    ];
+})
